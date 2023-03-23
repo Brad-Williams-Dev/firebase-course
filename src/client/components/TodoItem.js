82cdd927504836901d0c56cb1a8c35ca2f46b8c8
Brad Williams <bradley_williams2003@hotmail.com>
2023-03-23 11:07:10 -0300

    Add user update todo list item

--- src/client/components/TodoItem.js
+++ src/client/components/TodoItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { XIcon } from '@heroicons/react/outline';
+import React, { useState } from 'react';
+import { PencilIcon, XIcon, CheckIcon } from '@heroicons/react/outline';
 import { updateTodo, deleteTodo } from '../../firebase';
 
 const Todo = ({ item }) => {
   // const [checked, setChecked] = useState(item.done);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedText, setUpdatedText] = useState(item.name);
 
   const handleCheckbox = () => {
     updateTodo(item.id, { done: !item.done });
@@ -13,36 +16,76 @@ const Todo = ({ item }) => {
     deleteTodo(item.id);
   };
 
+  const handleEdit = () => {
+    if (isEditing) {
+      updateTodo(item.id, { name: updatedText });
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const handleSave = () => {
+    updateTodo(item.id, { name: updatedText });
+    setIsEditing(!isEditing);
+  };
+
   return (
     <fieldset>
       <legend className="sr-only">To-do item</legend>
-      <div className="flex justify-between items-center my-2 h-5">
-        <label
-          htmlFor="item"
-          className="flex items-center text-sm text-gray-700"
-        >
+      {isEditing ? (
+        <div className="flex justify-between items-center my-2 h-5">
           <input
-            id={item.id}
-            name="item"
-            type="checkbox"
-            checked={item.done}
-            onChange={handleCheckbox}
-            className="mr-3 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+            type="text"
+            value={updatedText}
+            className="border-2 border-gray-300 rounded-md p-1"
+            onChange={(e) => setUpdatedText(e.target.value)}
           />
-          {item.name}
-        </label>
-        <button
-          type="button"
-          onClick={handleDelete}
-          className="inline-flex items-center p-1 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-        >
-          <span className="sr-only">Delete Item</span>
-          <XIcon className="block h-4 w-4" aria-hidden="true" />
+          <button className='inline-flex mr-3 items-center p-1 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm' type="button" onClick={handleSave}>
+            <span className="sr-only">Save Item</span>
+            <CheckIcon className="block h-4 w-4" aria-hidden="true" />
+          </button>
+        </div>
+      ) :
+        (<div className="flex justify-between items-center my-2 h-5">
+          <label
+            htmlFor="item"
+            className="flex items-center text-sm text-gray-700"
+          >
+            <input
+              id={item.id}
+              name="item"
+              type="checkbox"
+              checked={item.done}
+              onChange={handleCheckbox}
+              className="mr-3 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+            />
+            {item.name}
+          </label>
+          <div className='flex flex-row'>
+            <button
+              type="button"
+              onClick={handleEdit}
+              className="inline-flex mr-3 items-center p-1 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            >
+              <span className="sr-only">Edit Item</span>
+              <PencilIcon className="block h-4 w-4" aria-hidden="true" />
+            </button>
+            <button
+              type="button"
+              onClick={handleDelete}
+              className="inline-flex items-center p-1 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            >
+              <span className="sr-only">Delete Item</span>
+              <XIcon className="block h-4 w-4" aria-hidden="true" />
+            </button>
+          </div>
+        </div>)
+      }
 
-        </button>
-      </div>
     </fieldset>
   );
 };
 
 export default Todo;
+
+
+
